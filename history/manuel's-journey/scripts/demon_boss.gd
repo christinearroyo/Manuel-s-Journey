b1633688a2b79501611374ceddf46e828d17ee4d
JayAld <resurreccionaldrin1@gmail.com>
2026-05-23 04:40:26 +0800

    f2

--- history/manuel's-journey/scripts/demon_boss.gd
+++ history/manuel's-journey/scripts/demon_boss.gd
@@ -7,6 +7,7 @@ const collision_distance = 11
 
 var direction = -1
 @onready var collision_shape_2d: CollisionShape2D = $Killzone/CollisionShape2D
+@onready var animated_sprite_2d_2: AnimatedSprite2D = $AnimatedSprite2D2
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
@@ -15,9 +16,11 @@ func _process(delta: float) -> void:
 		direction = -1
 		animated_sprite.flip_h = false
 		collision_shape_2d.position.x -= collision_distance*2
+		animated_sprite_2d_2.position.x -= collision_distance*2
 	if ray_cast_left.is_colliding():
 		direction = 1
 		animated_sprite.flip_h = true
 		collision_shape_2d.position.x += collision_distance*2
+		animated_sprite_2d_2.position.x += collision_distance*2
 	position.x += direction * SPEED * delta
 	
--- history/manuel's-journey/scripts/killzone.gd
+++ history/manuel's-journey/scripts/killzone.gd
@@ -6,6 +6,7 @@ func _on_body_entered(body):
 	print("You Died")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+	body.get_node("AnimatedSprite2D").play()
 	timer.start()
 	
 func _on_timer_timeout() -> void:
